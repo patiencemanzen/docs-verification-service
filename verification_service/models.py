f9f1fc14c371c3c5587eab302f3c3063c8748c17
Manirabona Patience <hseal419@gmail.com>
2024-10-11 16:26:36 +0200

    Refactor models and serializers in verification_service

--- verification_service/models.py
+++ verification_service/models.py
@@ -16,27 +16,8 @@ def upload_image_file(instance, filename):
     extension = filename.split('.')[-1]
     new_filename = f"murugo_image_{datetime.now().strftime('%Y%m%d_%H%M%S')}.{extension}"
     return os.path.join('uploaded_image_files/', new_filename)
-
-class UserProfile(models.Model):
-    murugo_user_id = models.CharField(max_length=100)
-    firstname = models.CharField(max_length=100)
-    secondname = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    role = models.CharField(max_length=100)
-    personalid = models.CharField(max_length=100, unique=True)
-    address = models.CharField(max_length=255)
-    city = models.CharField(max_length=100)
-    dob = models.DateField()
-    countryCode = models.CharField(max_length=10)
-    country = models.CharField(max_length=100)
-    phoneNumber = models.CharField(max_length=15)
-    id_type = models.CharField(max_length=50)
-
-    def __str__(self):
-        return f"{self.firstname} {self.secondname} ({self.email})"
     
 class UploadedFile(models.Model):
-    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='uploaded_files')
     file = models.FileField(upload_to=upload_id_file)
     file_hash = models.CharField(max_length=64, unique=True, blank=True, null=True)
     image_file = models.FileField(upload_to=upload_image_file)
