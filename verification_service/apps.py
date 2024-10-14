721210e06ddbcb085c244aa4785ea1fa4ecf15d2
Manirabona Patience <hseal419@gmail.com>
2024-10-14 14:36:18 +0200

    Refactor apps.py to initialize Celery worker only once

--- verification_service/apps.py
+++ verification_service/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig # type: ignore
-
+import subprocess
+import os
+import sys
 
 class VerificationServiceConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
@@ -8,3 +10,9 @@ class VerificationServiceConfig(AppConfig):
     def ready(self):
         from .services import DataExtractionService
         DataExtractionService.initChatSession()
+
+        # Ensure this runs only once
+        if os.environ.get('RUN_MAIN') == 'true' or 'runserver' not in sys.argv:
+            return
+
+        subprocess.Popen(['celery', '-A', 'gemini_verification_service', 'worker', '--loglevel=info'])
