978b57ec2d17b3b9a1795ce3a09bb18b2ab55b07
Manirabona Patience <hseal419@gmail.com>
2024-10-18 10:59:37 +0200

    Refactor Dockerfile and settings.py to update Redis connection URLs

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 # Exit on error
 set -o errexit
 
-# Modify this line as needed for your package manager (pip, poetry, etc.)
+# Install All required Packages from requirements.txt 
 pip install -r requirements.txt
 
 # Convert static asset files
